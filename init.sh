f1e194275c6d06fa8e6654867d9bf02ade6c7b7e
kdrygin <kdrygin@gmail.com>
2016-03-10 08:21:39 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/
+sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/
 sudo /etc/init.d/gunicorn restart
