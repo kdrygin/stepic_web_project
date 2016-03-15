9ee1d9faa193e7ef2a204413e4ce140f5033a5ea
kdrygin <kdrygin@gmail.com>
2016-03-15 16:06:32 +0600

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,13 +1,16 @@
 CONFIG = {
     'mode': 'wsgi',
-    'working_dir': '/home/box/web',
+    #'working_dir': '/home/box/web',
+    'working_dir': '/home/box/web/ask',
     # 'python': '/usr/bin/python',
     'args': (
         #'--bind=0.0.0.0:8080',
-        '--bind=127.0.0.1:8080',
+        # ver 1'--bind=127.0.0.1:8080',
+        '--bind=127.0.0.1:8000',
         '--daemon',
         '--workers=2',
         '--timeout=60',
-        'hello:app',
+        # ver 1 'hello:app',
+        'ask.wsgi:application',
     ),
 }
