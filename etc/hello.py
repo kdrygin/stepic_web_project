755607d4a0a9682c47f1ba586765ada70cc35a58
kdrygin <kdrygin@gmail.com>
2016-03-15 16:41:19 +0600

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -4,9 +4,9 @@ CONFIG = {
     'working_dir': '/home/box/web/ask',
     # 'python': '/usr/bin/python',
     'args': (
-        #'--bind=0.0.0.0:8080',
+        '--bind=0.0.0.0:8000',
         # ver 1'--bind=127.0.0.1:8080',
-        '--bind=127.0.0.1:8000',
+        #'--bind=127.0.0.1:8000',
         '--daemon',
         '--workers=2',
         '--timeout=60',
